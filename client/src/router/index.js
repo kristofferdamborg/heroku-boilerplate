e83c5b9f84d68ff9d8dffd6358b35d91f346e8e5
Kristoffer Damborg <kristofferdamborg@gmail.com>
2017-11-08 00:25:41 +0100

    Fixed typo

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Test from '@/componenets/Test'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
